2cbc1bd406474e7ceeb9e4fc76f3c1fed10646e5
GrizzlLearn <dylebedev.learning@gmail.com>
2022-11-26 16:28:44 +0300

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,64 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.previousIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+                iterator.add(value);
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> listIterator = list.listIterator();
+        for (T e : elements) {
+            while (listIterator.hasNext()) {
+                if (listIterator.next().equals(e)) {
+                    listIterator.remove();
+                    break;
+                }
+            }
+        }
+    }
+}
