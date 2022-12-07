967ae789dc2887923309b5e6a46772487f6214e4
GrizzlLearn <dylebedev.learning@gmail.com>
2022-12-07 22:36:05 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -14,7 +14,7 @@ public class User {
     }
 
     public static void main(String[] args) {
-        Calendar birth = new GregorianCalendar(1978, Calendar.MAY, 25);
+        Calendar birth = Calendar.getInstance();
         User user1 = new User("Slava", 1, birth);
         User user2 = new User("Slava", 1, birth);
         Map<User, Object> map = new HashMap<>(16);
@@ -23,17 +23,30 @@ public class User {
 
         int hashcode1 = user1.hashCode();
         int hash1 = hashcode1 ^ (hashcode1 >>> 16);
-        int backet1 = hash1 & 15;
+        int bucket1 = hash1 & 15;
 
         int hashcode2 = user2.hashCode();
         int hash2 = hashcode2 ^ (hashcode1 >>> 16);
-        int backet2 = hash2 & 15;
+        int bucket2 = hash2 & 15;
 
-        System.out.printf("user1 = хэшкод: %s, хэш: %s, backet: %s", hashcode1, hash1, backet1);
+        System.out.printf("user1 = хэшкод: %s, хэш: %s, backet: %s", hashcode1, hash1, bucket1);
         System.out.println(System.lineSeparator());
-        System.out.printf("user2 = хэшкод: %s, хэш: %s, backet: %s", hashcode2, hash2, backet2);
+        System.out.printf("user2 = хэшкод: %s, хэш: %s, backet: %s", hashcode2, hash2, bucket2);
         System.out.println(System.lineSeparator());
         System.out.println(map.get(user1));
         System.out.println(map.get(user2));
+    }
+
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }*/
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
