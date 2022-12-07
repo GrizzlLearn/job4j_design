02e95a075a31e9efd0ceb7f80da7e7997a9a764e
GrizzlLearn <dylebedev.learning@gmail.com>
2022-12-07 21:19:10 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,29 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar birth = new GregorianCalendar(1978, Calendar.MAY, 25);
+        User user1 = new User("Slava", 1, birth);
+        User user2 = new User("Slava", 1, birth);
+        Map<User, Object> map = new HashMap<>(16);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        int hashcode1 = user1.hashCode();
+        int hash1 = hashcode1 ^ (hashcode1 >>> 16);
+        int backet1 = hash1 & 15;
+
+        int hashcode2 = user2.hashCode();
+        int hash2 = hashcode2 ^ (hashcode1 >>> 16);
+        int backet2 = hash2 & 15;
+
+        System.out.printf("user1 = хэшкод: %s, хэш: %s, backet: %s", hashcode1, hash1, backet1);
+        System.out.println(System.lineSeparator());
+        System.out.printf("user2 = хэшкод: %s, хэш: %s, backet: %s", hashcode2, hash2, backet2);
+        System.out.println(System.lineSeparator());
+        System.out.println(map.get(user1));
+        System.out.println(map.get(user2));
     }
 }
