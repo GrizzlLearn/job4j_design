ed6150884fa98a1e04200b95eda0aafd9fb4c85c
GrizzlLearn <dylebedev.learning@gmail.com>
2023-03-16 21:05:48 +0300

    2.0. FileOutputStream [#252488] done

---
+++ src/main/java/ru/job4j/io/MathTab.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+public class MathTab {
+    public static String printMathTab() {
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <=10; j++) {
+                result.append(i * j);
+                if (j < 10) {
+                    result.append("\t");
+                }
+            }
+            result.append(System.lineSeparator());
+        }
+
+        return result.toString();
+    }
+}
