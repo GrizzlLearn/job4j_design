49b5bdb53c124e89eab62bf9f1d305d8143949aa
GrizzlLearn <dylebedev.learning@gmail.com>
2023-03-16 21:08:21 +0300

    2.0. FileOutputStream [#252488] fix style

--- src/main/java/ru/job4j/io/MathTab.java
+++ src/main/java/ru/job4j/io/MathTab.java
@@ -5,7 +5,7 @@ public class MathTab {
         StringBuffer result = new StringBuffer();
 
         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <=10; j++) {
+            for (int j = 1; j <= 10; j++) {
                 result.append(i * j);
                 if (j < 10) {
                     result.append("\t");
