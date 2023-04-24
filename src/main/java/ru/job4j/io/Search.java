3e3217217f9c275533beff3f1ca676aaaaaeb33e
GrizzlLearn <dylebedev.learning@gmail.com>
2023-04-24 20:01:08 +0300

    10. Сканирование файловой системы [#106929] done

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,60 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    private static class SearchFiles implements FileVisitor<Path> {
+        private Predicate<Path> condition;
+        private List<Path> found = new ArrayList<>();
+
+        SearchFiles(Predicate<Path> condition) {
+            this.condition = condition;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (condition.test(file)) {
+                found.add(file);
+            }
+
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+        
+        public List<Path> getPaths() {
+            return this.found;
+        }
+    }
+}
+
+
