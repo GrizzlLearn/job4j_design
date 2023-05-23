77c14c0c638eeb72577190f060b9cf137a9a839e
GrizzlLearn <dylebedev.learning@gmail.com>
2023-05-23 23:01:57 +0300

    WIP: 14. Архивировать проект [#861] do

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,7 +1,13 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -22,11 +28,35 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
-        zip.packSingleFile(
+
+        ArgsName name = ArgsName.of(args);
+        String[] test = new String[]{"d", "e", "o"};
+        Predicate<Path> predicate = p -> !p.toFile().getName().endsWith(name.get(test[1]));
+
+
+        List<File> lf = new ArrayList<>();
+
+        for (Path path : Search.search(Paths.get(name.get(test[0])), predicate)) {
+            lf.add(new File(path.getFileName().toUri()));
+        }
+
+        lf.forEach(System.out::println);
+
+        zip.packFiles(lf, new File("/Users/dvl/projects/job4j_design.zip"));
+
+
+        /*System.out.println(
+                Search.search(
+                        Paths.get(name.get(test[0])),
+                        predicate));*/
+
+        //Search.search(ArgsName.of(args).get(args[0]), p -> p.toFile().getName().endsWith(args[1]));
+
+        /*zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
-        );
+        );*/
     }
 }
