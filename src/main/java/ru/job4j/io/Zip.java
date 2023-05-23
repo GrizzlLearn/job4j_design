5e399dc2bb53341af4a341bc50ee2df81bb7219f
GrizzlLearn <dylebedev.learning@gmail.com>
2023-05-23 23:11:28 +0300

    WIP: 14. Архивировать проект [#861] do packFiles

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,10 +3,8 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -14,7 +12,16 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public void packFiles(List<File> sources, File target) {
-
+        sources.forEach(s -> {
+            try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+                zip.putNextEntry(new ZipEntry(s.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(s))) {
+                    zip.write(out.readAllBytes());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public void packSingleFile(File source, File target) {
@@ -35,16 +42,15 @@ public class Zip {
         String[] test = new String[]{"d", "e", "o"};
         Predicate<Path> predicate = p -> !p.toFile().getName().endsWith(name.get(test[1]));
 
-
         List<File> lf = new ArrayList<>();
 
         for (Path path : Search.search(Paths.get(name.get(test[0])), predicate)) {
-            lf.add(new File(path.getFileName().toUri()));
+            lf.add(new File(path.getFileName().toString()));
         }
 
         lf.forEach(System.out::println);
 
-        zip.packFiles(lf, new File("/Users/dvl/projects/job4j_design.zip"));
+        zip.packFiles(lf, new File("../job4j_design.zip"));
 
 
         /*System.out.println(
@@ -55,8 +61,8 @@ public class Zip {
         //Search.search(ArgsName.of(args).get(args[0]), p -> p.toFile().getName().endsWith(args[1]));
 
         /*zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
+                new File("pom.xml"),
+                new File("pom.zip")
         );*/
     }
 }
