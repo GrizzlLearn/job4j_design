e36c29b5d5c417dff08f5143da5e42f3389768da
GrizzlLearn <dylebedev.learning@gmail.com>
2023-05-25 00:28:10 +0300

    WIP: 14. Архивировать проект [#861] fix issues & add params validation

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -10,48 +10,62 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            sources.forEach(s -> {
-                try {
-                    zip.putNextEntry(new ZipEntry(s.getPath()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            for (File s : sources) {
+                zip.putNextEntry(new ZipEntry(s.getPath()));
                 try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(s))) {
                     zip.write(out.readAllBytes());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    };
-
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private static boolean validateRoot(String root) {
+        File file = new File(root);
+
+        if (!file.exists()) {
+            throw new IllegalArgumentException("You must set EXIST ROOT DIRECTORY.");
+        }
+
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException("You must set ROOT DIRECTORY, not FILE.");
+        }
+
+        return true;
+    }
+
+    private static boolean validateTarget(File target) {
+
+        if (target.exists()) {
+            throw new IllegalArgumentException("A file with the same name already EXISTS.");
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
         ArgsName name = ArgsName.of(args);
         List<File> lf = new ArrayList<>();
-        String[] parameterNames = new String[]{"d", "e", "o"};
-        Predicate<Path> predicate = p -> !p.toFile().getName().endsWith(name.get(parameterNames[1]));
+        String[] parametersArray = new String[]{"d", "e", "o"};
+        Predicate<Path> predicate = p -> !p.toFile().getName().endsWith(name.get(parametersArray[1]));
+        String root = name.get(parametersArray[0]);
+        File target = new File("../job4j_design.zip");
 
-        for (Path path : Search.search(Paths.get(name.get(parameterNames[0])), predicate)) {
-            lf.add(path.toFile());
+        if (validateRoot(root)) {
+            for (Path path : Search.search(Paths.get(root), predicate)) {
+                lf.add(path.toFile());
+            }
         }
 
-        zip.packFiles(lf, new File("../job4j_design.zip"));
+        if (validateTarget(target)) {
+            zip.packFiles(lf, target);
+            System.out.println("Archiving is complete!");
+        }
     }
 }
