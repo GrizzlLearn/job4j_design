f3bcce4207405c6549effadace4e025aba779c4b
GrizzlLearn <dylebedev.learning@gmail.com>
2023-05-31 23:59:50 +0300

    17. Scanner [#504791]: do

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,19 +1,62 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import java.io.*;
+import java.util.*;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+        File file = new File(argsName.get("path"));
+        String filterDelimiter = ",";
+        String delimiter = argsName.get("delimiter");
+        List<String> filter = new ArrayList<>(Arrays.stream(argsName.get("filter").split(filterDelimiter)).toList());
+        List<String> tmp = new ArrayList<>();
+        List<List<String>> finalList = new ArrayList<>();
+
+        StringBuilder result = new StringBuilder();
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNext()) {
+            for (String s : scanner.nextLine().split(System.lineSeparator())) {
+                finalList.add(new ArrayList<>(List.of(s.split(delimiter))));
+                if (s.contains(filter.get(0))) {
+                    tmp.addAll(List.of(s.split(delimiter)));
+                }
+            }
+        }
+
+        for (List<String> stringList : finalList) {
+            for (int i = 0; i < filter.size(); i++ ) {
+                result.append(stringList.get(tmp.indexOf(filter.get(i))));
+
+                if (i + 1 < filter.size()) {
+                    result.append(delimiter);
+                }
+            }
+            result.append(System.lineSeparator());
+        }
+
+        System.out.println(result);
     }
 
-    public static void main(String[] args) {
+    private static boolean validateArgs(ArgsName args) {
+        File file = new File(args.get("path"));
+
+        if (!file.exists()) {
+            throw new IllegalArgumentException("You must set EXIST FILE.");
+        }
+
+        if (!file.isFile()) {
+            throw new IllegalArgumentException("You must set FILE, not DIRECTORY.");
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
         /* здесь добавьте валидацию принятых параметров*/
         ArgsName argsName = ArgsName.of(args);
-        handle(argsName);
+        if (validateArgs(argsName)) {
+            handle(argsName);
+        }
     }
 }
