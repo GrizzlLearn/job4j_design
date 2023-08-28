c3d1ea7314d5e850d38cbc99fe22e22598531844
GrizzlLearn <dylebedev.learning@gmail.com>
2023-08-29 01:02:41 +0300

    0.2. PreparedStatement [#379307] some fixes

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -17,45 +17,24 @@ public class ImportDB {
     /**
      * Подготавливает базу данных с указанным именем и таблицей для использования.
      *
-     * @param dbName Имя базы данных, которая должна быть создана.
      * @param tableName Имя таблицы в базе данных, которая должна быть создана.
      * @throws ClassNotFoundException Если драйвер базы данных не может быть найден.
-     * @throws SQLException Если происходит ошибка при создании базы данных или таблицы.
+     * @throws SQLException           Если происходит ошибка при создании базы данных или таблицы.
      */
-    static private void prepareDB(String dbName, String tableName) throws ClassNotFoundException, SQLException {
-        String createDB = String.format("CREATE DATABASE %s;",
-                dbName
-        );
+    static private void prepareDB(String tableName) throws ClassNotFoundException, SQLException {
         String createTable = String.format("CREATE TABLE IF NOT EXISTS %s(%s, %s, %s);",
                 tableName,
                 "id serial primary key",
                 "username VARCHAR(255) NOT NULL",
                 "email VARCHAR(255) NOT NULL"
-                );
-        List<String> sqlList = new ArrayList<>() {{
-            add(createDB);
-            add(createTable);
-        }};
+        );
         try (Connection connection = getConnection(cfg)) {
-            for (String st : sqlList) {
-                try (Statement s = connection.createStatement()) {
-                    if (st.startsWith("CREATE DATABASE")) {
-                        if (!checkDatabaseExists(s, dbName)) {
-                            s.execute(st);
-                        }
-                    } else {
-                        s.execute(st);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+            try (Statement s = connection.createStatement()) {
+                s.execute(createTable);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
-    }
-
-    private static boolean checkDatabaseExists(Statement statement, String dbName) throws SQLException {
-        String query = "SELECT datname FROM pg_database WHERE datname = '" + dbName + "'";
-        return statement.executeQuery(query).next();
     }
 
     /**
@@ -64,10 +43,11 @@ public class ImportDB {
      * @param cfg Настройки для установки соединения с базой данных.
      * @return Объект Connection, представляющий соединение с базой данных.
      * @throws ClassNotFoundException Если драйвер базы данных не может быть найден.
-     * @throws SQLException Если происходит ошибка при установке соединения.
+     * @throws SQLException           Если происходит ошибка при установке соединения.
      */
     private static Connection getConnection(Properties cfg) throws ClassNotFoundException, SQLException {
         Class.forName(cfg.getProperty("jdbc.driver"));
+
         return DriverManager.getConnection(
                 cfg.getProperty("jdbc.url"),
                 cfg.getProperty("jdbc.username"),
@@ -75,20 +55,17 @@ public class ImportDB {
         );
     }
 
-    //TODO
-    // Сделать проверку, что в массиве точно 2 элемента и они не пустые
-    // Если не проходит – выбрасывайте IllegalArgumentException
-
     /**
      * Формирует список пользователей.
+     *
      * @return Список пользователей.
-     * @throws IOException если ошибка чтения конфига.
+     * @throws IOException              если ошибка чтения конфига.
      * @throws IllegalArgumentException если ошибка добавления пользователя в список.
      */
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            rd.lines().forEach( line -> {
+            rd.lines().forEach(line -> {
                         String[] tmp = line.split(";");
                         if (tmp.length > 1 && tmp[0].length() > 0 && tmp[1].length() > 0) {
                             users.add(new User(tmp[0], tmp[1]));
@@ -98,15 +75,17 @@ public class ImportDB {
                     }
             );
         }
+
         return users;
     }
 
     /**
      * Сохраняет имеющихся пользователь в указанной таблице.
-     * @param users список пользователей, сформированный see #load() .
+     *
+     * @param users     список пользователей, сформированный see #load() .
      * @param tableName название таблицы в которую необходимо сохранить пользователей.
      * @throws ClassNotFoundException Если драйвер базы данных не может быть найден.
-     * @throws SQLException Если происходит ошибка при сохранении данных в таблицу.
+     * @throws SQLException           Если происходит ошибка при сохранении данных в таблицу.
      */
     public void save(List<User> users, String tableName) throws ClassNotFoundException, SQLException {
         String sql = String.format("INSERT INTO %s(username, email) VALUES(?, ?)",
@@ -135,16 +114,14 @@ public class ImportDB {
         }
     }
 
-
     public static void main(String[] args) throws Exception {
         Properties cfg = new Properties();
-        String dbName = "spammer";
         String tableName = "spammer_table";
         try (InputStream in = ImportDB.class.getClassLoader().getResourceAsStream("app.properties")) {
             cfg.load(in);
         }
         ImportDB db = new ImportDB(cfg, "./src/main/java/ru/job4j/spammer/dump.txt");
-        prepareDB(dbName, tableName);
+        prepareDB(tableName);
         db.save(db.load(), tableName);
     }
 }
