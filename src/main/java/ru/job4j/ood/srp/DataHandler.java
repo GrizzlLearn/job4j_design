64e5e230423d700a8dd404a127c70cad63271f04
dl <dl@air.local>
2023-12-03 18:35:01 +0300

    0. Принцип единственной ответственности [#4913] done

---
+++ src/main/java/ru/job4j/ood/srp/DataHandler.java
@@ -0,0 +1,16 @@
+package ru.job4j.ood.srp;
+
+public class DataHandler {
+
+    /**
+     * Чтение данных из файла
+     */
+    public void readData() {
+    }
+
+    /**
+     * Запись данных в файл
+     */
+    public void writeData() {
+    }
+}
